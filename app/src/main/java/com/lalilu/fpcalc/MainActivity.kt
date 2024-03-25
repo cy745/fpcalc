b3f96cda2184c784dc61d05061256454d242215a
lalilu <1248393538@qq.com>
2024-03-26 02:29:07 +0800

    修改读取测试的逻辑，避免直接读取raw文件夹内的文件的fd用于计算音频指纹

--- app/src/main/java/com/lalilu/fpcalc/MainActivity.kt
+++ app/src/main/java/com/lalilu/fpcalc/MainActivity.kt
@@ -2,12 +2,13 @@ package com.lalilu.fpcalc
 
 import android.os.Bundle
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.blankj.utilcode.util.FileIOUtils
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,15 +16,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         GlobalScope.launch(Dispatchers.IO) {
-            val result = runCatching {
-                val fd = resources.openRawResourceFd(R.raw.test).parcelFileDescriptor.detachFd()
+            val tempFile = File(cacheDir, "tempAudio")
+            val inputStream = resources.openRawResource(R.raw.test)
+            FileIOUtils.writeFileFromIS(tempFile, inputStream)
+            // 使用openRawResourceFd获取到的fd用于Fpcalc进行读取会导致MediaExtractor创建失败
+            // 故先通过读取流后，写入到cache文件夹中，再将该文件的path用于读取测试
 
-                Fpcalc.calc(arrayOf("-raw", "$fd"))
-            }.getOrNull()
+            val params = FpcalcParams(targetFilePath = tempFile.path)
+            val result = Fpcalc.calc(params)
 
             withContext(Dispatchers.Main) {
                 this@MainActivity.findViewById<TextView>(R.id.result).text = "$result"
-                Toast.makeText(this@MainActivity, result.toString(), Toast.LENGTH_SHORT).show()
             }
         }
     }
