1cb671557a0b46bfb239d863120ce49eba8eb2f4
lalilu <1248393538@qq.com>
2024-03-26 13:00:17 +0800

    添加重试按钮

--- app/src/main/java/com/lalilu/fpcalc/MainActivity.kt
+++ app/src/main/java/com/lalilu/fpcalc/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.lalilu.fpcalc
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.blankj.utilcode.util.FileIOUtils
@@ -15,6 +17,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        findViewById<Button>(R.id.retry).setOnClickListener {
+            calc()
+        }
+
+        calc()
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun calc() {
         GlobalScope.launch(Dispatchers.IO) {
             val tempFile = File(cacheDir, "tempAudio")
             val inputStream = resources.openRawResource(R.raw.test)
@@ -22,11 +33,16 @@ class MainActivity : AppCompatActivity() {
             // 使用openRawResourceFd获取到的fd用于Fpcalc进行读取会导致MediaExtractor创建失败
             // 故先通过读取流后，写入到cache文件夹中，再将该文件的path用于读取测试
 
-            val params = FpcalcParams(targetFilePath = tempFile.path)
-            val result = Fpcalc.calc(params)
+            val params: FpcalcParams = FpcalcParams(targetFilePath = tempFile.path)
+            val result: FpcalcResult = Fpcalc.calc(params)
+
+            val startTime = System.currentTimeMillis()
 
             withContext(Dispatchers.Main) {
-                this@MainActivity.findViewById<TextView>(R.id.result).text = "$result"
+                this@MainActivity.findViewById<TextView>(R.id.result).text = """
+                    startTime: $startTime
+                    result:$result
+                """.trimIndent()
             }
         }
     }
